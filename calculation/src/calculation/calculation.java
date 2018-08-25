d9a578efda2484528c399bb46498aa0aff9e4491
sridevasena <40741324+sridevasena@users.noreply.github.com>
2018-08-25 15:15:16 -0400

    intial commit

---
+++ calculation/src/calculation/calculation.java
@@ -0,0 +1,16 @@
+package calculation;
+
+public class calculation {
+
+	void fact(int  n){  
+			  int fact=1;  
+			  for(int i=1;i<=n;i++){  
+			   fact=fact*i;  
+		  }  
+			 System.out.println("factorial is "+fact);  
+			}  
+			public static void main(String args[]){  
+			 new calculation().fact(5);//calling method with anonymous object  
+			}  
+			}  
+
